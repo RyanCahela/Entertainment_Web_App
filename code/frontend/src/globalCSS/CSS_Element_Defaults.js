85d6cb2c0d2bb65f1362a725abc7f708fc533b81
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2022-03-22 11:43:11 -0700

    added box-sizing: border-box to all elemnts

--- code/frontend/src/globalCSS/CSS_Element_Defaults.js
+++ code/frontend/src/globalCSS/CSS_Element_Defaults.js
@@ -2,6 +2,14 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalCSSElementDefaults = createGlobalStyle`
   /* ! temporary for design system */
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+
 body {
   background-color: var(--clr-black);
   color: var(--clr-white);
