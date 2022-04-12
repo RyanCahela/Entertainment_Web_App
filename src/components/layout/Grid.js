421cb892f232da66226a4efa1656eea3103c96b1
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2022-04-12 12:07:08 -0700

    added minimum grid sizes for MediaCard Grid

--- src/components/layout/Grid.js
+++ src/components/layout/Grid.js
@@ -2,15 +2,18 @@ import styled from "styled-components";
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(2, minmax(164px, 1fr));
+  grid-auto-rows: minmax(110px, auto);
   gap: 1rem;
 
   @media screen and (min-width: 768px) {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(3, minmax(220px, 1fr));
+    grid-auto-rows: minmax(140px, auto);
   }
 
   @media screen and (min-width: 1000px) {
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(4, minmax(280px, 1fr));
+    grid-auto-rows: minmax(174px, auto);
   }
 `;
 
